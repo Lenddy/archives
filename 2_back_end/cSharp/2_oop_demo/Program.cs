d14d84dce37cb670266178f3c03797fbb0073455
Lenddy <leme2003@gmail.com>
2023-09-18 08:54:12 -0400

    oop demo

--- 2_back_end/cSharp/2_oop_demo/Program.cs
+++ 2_back_end/cSharp/2_oop_demo/Program.cs
@@ -1,42 +1,40 @@
 ﻿using oopDemo;
 
-// list of courses
+//? list of courses
 List<string> courses = new List<string>();
 
-// person instance(object)
+//? person instance(object)
 Person person = new Person("alex","miller");
 
-// student instance(object)
+//? student instance(object)
 Student student = new Student("Lenddy","Morales",1 );
 Student student2 = new Student("student2","student2",2 );
 Student student3 = new Student("student3","student3",3 );
 Student student4 = new Student("student4","student4",4 );
 
-// Instructor instance(object)
+//? Instructor instance(object)
 Instructor instructor = new Instructor("max","min",courses);
 
-// adding to the courses List<>
+//? adding to the courses List<>
 courses.Add("python");
 courses.Add("javascript");
 courses.Add("c#");
 
-// List of students instances(objects)
+//? List of students instances(objects)
 List<Student>students = new List<Student>();
 
-// adding student  instances(objs) to the students list<>
+//? adding student  instances(objs) to the students list<>
 students.Add(student);
 students.Add(student2);
 students.Add(student3);
 students.Add(student4);
 
-// Lecture instance(obj)
+//? Lecture instance(obj)
 Lecture cSharp = new Lecture(instructor,students,"csharp");
 
-// calling and logging the instances(objs) methods
+//? calling and logging the instances(objs) methods
 Console.WriteLine(person.fullName());
 Console.WriteLine(student.fullName());
 Console.WriteLine( student.StudentID);
 instructor.courses();
 cSharp.attendance();
-
-
