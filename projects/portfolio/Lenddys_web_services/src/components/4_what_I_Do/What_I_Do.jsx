775acf9ea74d173f4fbddae48ae44b649dfc25b9
Lenddy <leme2003@gmail.com>
2023-09-29 02:47:25 -0400

    what i do

--- projects/portfolio/Lenddys_web_services/src/components/4_what_I_Do/What_I_Do.jsx
+++ projects/portfolio/Lenddys_web_services/src/components/4_what_I_Do/What_I_Do.jsx
@@ -3,26 +3,38 @@ const What_I_Do = () => {
 		<div>
 			<div className="whatIDo">
 				<div className="whatIDo_top">
-					{" "}
-					make this have a banner on top that changes from What do i
+					<div className="text">
+						<h1>What I Do</h1>
+						{/* make this appear with a animation read bellow */}
+						{/* <h1>full-stack engineer</h1>
+						<h1>pc building</h1> */}
+					</div>
+
+					{/* make this have a banner on top that changes from What do i
 					do --
 					{">"} full-stack engineer --{">"} pc building --{">"}{" "}
 					instructor (have a blur effect that follow the words and
 					make the word appear from the left go past the middle of the
 					screen then come back the blur effect should also do the
-					same but delayed )
+					same but delayed ) */}
 				</div>
 
 				<div className="whatIDo_middle">
-					<div className="whatIDo_middle"></div>
-					<div className="whatIDo_middle"></div>
+					<div className="whatIDo_left">
+						{" "}
+						<h1>left</h1>
+					</div>
+					<div className="whatIDo_right">
+						<h1>right</h1>
+					</div>
+					<div className="whatIDo_bottom">
+						<h1>bottom</h1>
+					</div>
 				</div>
 
-				<div className="whatIDo_bottom"></div>
-
-				<ol>
+				{/* <ol>
 					<li>
-						i an a full stack web developer my job is to make the
+						i am a full stack web developer my job is to make the
 						most perfect websites starting from you
 						view,functionality and how the data is store in a db
 						(keep adding to this later one )
@@ -33,7 +45,7 @@ const What_I_Do = () => {
 						talk abut teaching(instructing) and researching
 						technologies that the user might be interested on
 					</li>
-				</ol>
+				</ol> */}
 			</div>
 		</div>
 	);
