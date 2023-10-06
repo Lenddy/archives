10d6d52b516d95a1efe7a2bbc6c3f4ed245a5144
Lenddy <leme2003@gmail.com>
2023-10-06 01:38:10 -0400

    update

--- projects/portfolio/Lenddys_web_services/src/App.jsx
+++ projects/portfolio/Lenddys_web_services/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Container from "./components/Container";
+// import Test from "./components/Test";
 
 function App() {
 	return (
 		<>
 			<Container></Container>
+			{/* <Test></Test> */}
 		</>
 	);
 }
